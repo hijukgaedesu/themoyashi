c6001bd439e43073a3693e51163228772b402384
nomatterwhat <nakuna1129@gmail.com>
2026-07-22 18:24:53 +0900

    Add files via upload

---
+++ src/routes/index.tsx
@@ -0,0 +1,26 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { useEffect } from "react";
+
+export const Route = createFileRoute("/")({
+  component: Index,
+});
+
+function Index() {
+  useEffect(() => {
+    window.location.replace("/app/index.html");
+  }, []);
+  return (
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        minHeight: "100vh",
+        fontFamily: "system-ui, sans-serif",
+        color: "#8B6470",
+      }}
+    >
+      글쓰기 도우미로 이동 중…
+    </div>
+  );
+}
